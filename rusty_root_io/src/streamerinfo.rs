3d795ae078d8e8b41fcf8a88b1581b18df4c4147
Ho Chun Lau <39511811+kylelau519@users.noreply.github.com>
2026-03-01 12:24:39 -0800

    successfully decompress streamerinfo

--- rusty_root_io/src/streamerinfo.rs
+++ rusty_root_io/src/streamerinfo.rs
@@ -1,8 +1,8 @@
+use crate::compression::decompress;
 use crate::constant::{K_BYTECOUNTMASK, K_NEWCLASSTAG};
 use crate::tkey::TKey;
 use crate::tlist::TList;
 use crate::tstreamerinfo::TStreamerInfo;
-use byteorder::ReadBytesExt;
 use std::fs::File;
 use std::io;
 use std::io::{BufReader, Seek, SeekFrom};
@@ -14,7 +14,18 @@ pub struct StreamerInfo {
     pub tlist: TList<TStreamerInfo>,
 }
 
-impl StreamerInfo {}
+impl StreamerInfo {
+    // pub fn read_streamer_info_at(reader: &mut BufReader<File>, offset: u64) -> io::Result<Self> {
+    //     reader.seek(SeekFrom::Start(offset))?;
+    //     let streamer_info_header = TKey::read_tkey(reader)?;
+    //     let payload_len =
+    //         (streamer_info_header.n_bytes - streamer_info_header.key_len as u32) as usize;
+    //     let mut payload = vec![0u8; payload_len];
+    //     let mut data = decompress(&payload, 100);
+    //     assert_eq!(data.len(), streamer_info_header.obj_len as usize);
+
+    // }
+}
 
 #[cfg(test)]
 mod tests {
@@ -32,5 +43,38 @@ mod tests {
             .expect("Failed to read TKey at offset");
         assert_eq!(key.title, "Doubly linked list");
         dbg!(&key);
+    }
+
+    use crate::compression::decompress;
+    use byteorder::ReadBytesExt;
+    use std::io::{Read, Seek, SeekFrom};
+    #[test]
+    fn test_decode_streamer_info() {
+        let path =
+            "/Users/kylelau519/Programming/rusty_root/rusty_root_io/testfiles/wzqcd_mc20a.root";
+        let streamer_info_offset = 80357582;
+        let file = File::open(path).expect("Failed to open ROOT file");
+        let mut reader = BufReader::new(file);
+
+        let obj_len: usize = 30257;
+        let n_bytes: usize = 8360;
+        let key_len: usize = 64;
+        let mut data = vec![0u8; n_bytes - key_len as usize];
+        reader
+            .seek(SeekFrom::Start(streamer_info_offset + key_len as u64))
+            .expect("Failed to seek to compressed data");
+        reader
+            .read_exact(&mut data)
+            .expect("Failed to read compressed data");
+
+        // Decompress the data
+        let decompressed_data = decompress(&data, 101).expect("Failed to decompress data");
+        assert_eq!(decompressed_data.len(), obj_len as usize);
+        let mut decompressed_reader = BufReader::new(&decompressed_data[..]);
+
+        // let tlist: TList<()> = TList::read_tlist_metadata_at(&mut decompressed_reader, 0)
+        //     .expect("Failed to read the tlist");
+
+        // dbg!(&tlist);
     }
 }
--- rusty_root_io/src/tlist.rs
+++ rusty_root_io/src/tlist.rs
@@ -1,12 +1,14 @@
+use crate::tobject::TObject;
 use crate::utils;
 use byteorder::ReadBytesExt;
 use std::fs::File;
-use std::io::{BufReader, Seek, SeekFrom};
+use std::io::{BufReader, Read, Seek, SeekFrom};
 
 #[derive(Default, Debug)]
 pub struct TList<T> {
     pub byte_count: u32,
     pub version: u16,
+    pub tobject: TObject,
     pub f_name_byte: u8,
     pub f_name: String,
     pub n_objects: u32,
@@ -18,12 +20,14 @@ impl TList<()> {
         reader.seek(SeekFrom::Start(offset))?;
         let byte_count = reader.read_u32::<byteorder::BigEndian>()?;
         let version = reader.read_u16::<byteorder::BigEndian>()?;
+        let tobject = TObject::read_tobject(reader)?;
         let f_name_byte = reader.read_u8()?;
         let f_name = utils::read_string(reader, f_name_byte as usize)?;
         let n_objects = reader.read_u32::<byteorder::BigEndian>()?;
         Ok(TList {
             byte_count,
             version,
+            tobject,
             f_name_byte,
             f_name,
             n_objects,
@@ -33,5 +37,27 @@ impl TList<()> {
     pub fn read_tlist(reader: &mut BufReader<File>) -> std::io::Result<Self> {
         let loc = reader.seek(SeekFrom::Current(0))?;
         Self::read_tlist_at(reader, loc)
+    }
+
+    pub fn read_tlist_metadata_at(
+        reader: &mut BufReader<File>,
+        offset: u64,
+    ) -> std::io::Result<Self> {
+        reader.seek(SeekFrom::Start(offset))?;
+        let byte_count = reader.read_u32::<byteorder::BigEndian>()?;
+        let version = reader.read_u16::<byteorder::BigEndian>()?;
+        let tobject = TObject::read_tobject(reader)?;
+        let f_name_byte = reader.read_u8()?;
+        let f_name = utils::read_string(reader, f_name_byte as usize)?;
+        let n_objects = reader.read_u32::<byteorder::BigEndian>()?;
+        Ok(TList {
+            byte_count,
+            version,
+            tobject,
+            f_name_byte,
+            f_name,
+            n_objects,
+            objects: Vec::new(), // Placeholder, as we don't know the type T or how to read it yet
+        })
     }
 }
