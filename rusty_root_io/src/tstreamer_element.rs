a5974d91e067d3e5bf75375075075f1fb8637718
Ho Chun Lau <39511811+kylelau519@users.noreply.github.com>
2026-03-01 11:14:37 -0800

    Implement TStreamer type and element readers
    Add TStreamerType::read_streamer_type and
    TStreamerElement::read_tstreamer_element
    with parsing for multiple streamer classes. Support ClassInfo::Offset by
    seeking to
    start_pos + offset + 4 to read the class name and restore the reader
    position.
    Import std::io::Read for read_exact usage.

--- rusty_root_io/src/tstreamer_element.rs
+++ rusty_root_io/src/tstreamer_element.rs
@@ -3,7 +3,7 @@ use crate::utils::ClassInfo;
 use byteorder::ReadBytesExt;
 use std::fs::File;
 use std::io;
-use std::io::BufReader;
+use std::io::{BufReader, Read};
 
 #[derive(Debug, Default)]
 pub struct TStreamerElementBase {
@@ -89,11 +89,102 @@ pub struct TStreamerElement {
     pub tstreamer_type: TStreamerType,
 }
 
-// impl TStreamerElement {
-//     pub fn read_tstreamer_element(reader: &mut BufReader<File>) -> io::Result<Self> {
-//         let byte_count = reader.read_u32::<byteorder::BigEndian>()?;
-//         let class_info = ClassInfo::read_class_info(reader)?;
-//         let remaining_bytes = reader.read_u32::<byteorder::BigEndian>()?;
-//         let version = reader.read_u16::<byteorder::BigEndian>()?;
-//         let tstreamer_element_base = TStreamerElementBase::read_tstreamer_element_base(reader)?;
-//         let tstreamer_type =
+impl TStreamerType {
+    pub fn read_streamer_type(reader: &mut BufReader<File>, class_name: &str) -> io::Result<Self> {
+        match class_name {
+            "TStreamerBase" => {
+                let base_version = reader.read_u32::<byteorder::BigEndian>()?;
+                Ok(TStreamerType::TStreamerInfoBase { base_version })
+            }
+            "TStreamerBasicType" => Ok(TStreamerType::TStreamerBasicType),
+            "TStreamerString" => Ok(TStreamerType::TStreamerString),
+            "TStreamerBasicPointer" => {
+                let count_version = reader.read_u32::<byteorder::BigEndian>()?;
+                let l_name = reader.read_u8()?;
+                let name = crate::utils::read_string(reader, l_name as usize)?;
+                let l_class_name = reader.read_u8()?;
+                let class_name_str = crate::utils::read_string(reader, l_class_name as usize)?;
+                Ok(TStreamerType::TStreamerBasicPointer {
+                    count_version,
+                    l_name,
+                    name,
+                    l_class_name,
+                    class_name: class_name_str,
+                })
+            }
+            "TStreamerObject" => Ok(TStreamerType::TStreamerObject),
+            "TStreamerObjectPointer" => Ok(TStreamerType::TStreamerObjectPointer),
+            "TStreamerLoop" => {
+                let count_version = reader.read_u32::<byteorder::BigEndian>()?;
+                let l_name = reader.read_u8()?;
+                let name = crate::utils::read_string(reader, l_name as usize)?;
+                let l_class_name = reader.read_u8()?;
+                let class_name_str = crate::utils::read_string(reader, l_class_name as usize)?;
+                Ok(TStreamerType::TStreamerLoop {
+                    count_version,
+                    l_name,
+                    name,
+                    l_class_name,
+                    class_name: class_name_str,
+                })
+            }
+            "TStreamerObjectAny" => Ok(TStreamerType::TStreamerObjectAny),
+            "TStreamerSTL" => {
+                let stl_type = reader.read_u32::<byteorder::BigEndian>()?;
+                let c_type = reader.read_u32::<byteorder::BigEndian>()?;
+                Ok(TStreamerType::TStreamerSTL { stl_type, c_type })
+            }
+            "TStreamerSTLString" => Ok(TStreamerType::TStreamerSTLString),
+            _ => Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                format!("Unknown TStreamerType class name: {}", class_name),
+            )),
+        }
+    }
+}
+
+impl TStreamerElement {
+    pub fn read_tstreamer_element(
+        reader: &mut BufReader<File>,
+        start_pos: u64,
+    ) -> io::Result<Self> {
+        use std::io::Seek;
+        let byte_count = reader.read_u32::<byteorder::BigEndian>()?;
+        let class_info = ClassInfo::read_class_info(reader)?;
+
+        let class_name = match &class_info {
+            ClassInfo::NewClass(name) => name.clone(),
+            ClassInfo::Offset(offset) => {
+                let current_pos = reader.stream_position()?;
+                // The offset points to the tag (4 bytes), so we skip it to read the string
+                reader.seek(std::io::SeekFrom::Start(start_pos + *offset as u64 + 4))?;
+                let mut name_bytes = Vec::new();
+                let mut byte = [0u8; 1];
+                loop {
+                    reader.read_exact(&mut byte)?;
+                    if byte[0] == 0 {
+                        break;
+                    }
+                    name_bytes.push(byte[0]);
+                }
+                // Seek back to continue reading
+                reader.seek(std::io::SeekFrom::Start(current_pos))?;
+                String::from_utf8_lossy(&name_bytes).into_owned()
+            }
+        };
+
+        let remaining_bytes = reader.read_u32::<byteorder::BigEndian>()?;
+        let version = reader.read_u16::<byteorder::BigEndian>()?;
+        let tstreamer_element_base = TStreamerElementBase::read_tstreamer_element_base(reader)?;
+        let tstreamer_type = TStreamerType::read_streamer_type(reader, &class_name)?;
+
+        Ok(Self {
+            byte_count,
+            class_info,
+            remaining_bytes,
+            version,
+            tstreamer_element_base,
+            tstreamer_type,
+        })
+    }
+}
